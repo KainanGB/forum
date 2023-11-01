2265c7c6f8bb273118650a30c15c494f15f20c7f
kainan.santos <kainanytbr@gmail.com>
2023-11-01 07:26:06 -0300

    chore: remove spring security flag

--- src/main/java/forum/forum/ForumApplication.java
+++ src/main/java/forum/forum/ForumApplication.java
@@ -2,9 +2,8 @@ package forum.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class ForumApplication {
 
 	public static void main(String[] args) {
